9cad46ac45a3f56dc20d3d0f4ab921340a7f6d6d
Jeffrey <youngjt@live.unc.edu>
2016-12-06 22:39:56 -0500

    a shameful solution to day 2 part 2

--- src/day2/Runner.java
+++ src/day2/Runner.java
@@ -6,21 +6,24 @@ import java.io.FileReader;
 public class Runner {
 
   private static int     _key   = 5;
-  private static int[][] keypad = new int[][] { { 1 }, { 2, 3, 4 }, { 5, 6, 7, 8, 9 }, { 10, 11, 12 }, { 13 } };
+  private static int[][] keypad = new int[][] { { 1 }, { 2, 3, 4 }, { 5, 6, 7, 8, 9 },
+      { 10, 11, 12 }, { 13 } };
 
   public static void main(String[] args) {
-    for (int i = 0; i < keypad.length; i++) {
-      for (int j = 0; j < keypad[0].length; j++) {
-        System.out.println("key: " + keypad[i][j] + " " + mapKeyToIndices(keypad[i][j])[0] + " " + mapKeyToIndices(keypad[i][j])[1]);
-      }
-    }
+    // for (int i = 0; i < keypad.length; i++) {
+    // for (int j = 0; j < keypad[0].length; j++) {
+    // System.out.println("key: " + keypad[i][j] + " " +
+    // mapKeyToIndices(keypad[i][j])[0]
+    // + " " + mapKeyToIndices(keypad[i][j])[1]);
+    // }
+    // }
     try {
       BufferedReader reader = new BufferedReader(new FileReader("input/day2input1.txt"));
       // BufferedReader reader = new BufferedReader(new
       // FileReader("input/test.txt"));
       while (reader.ready()) {
         String line = reader.readLine();
-        System.out.print(computeKey(line));
+        System.out.println(computeKey(line));
       }
     } catch (Exception e) {
       e.printStackTrace();
@@ -32,25 +35,117 @@ public class Runner {
     for (int i = 0; i < line.length(); i++) {
       direction = line.charAt(i);
       if (direction == 'U') {
-        int[] indices = mapKeyToIndices(_key - 3);
-        _key = keypad[indices[0]][indices[1]];
+        if (_key == 1) {
+          continue;
+        } else if (_key == 2) {
+          continue;
+        }  else if (_key == 3) {
+          _key = 1;
+        } else if (_key == 4) {
+          continue;
+        } else if (_key == 5) {
+          continue;
+        } else if (_key == 6) {
+          _key = 2;
+        } else if (_key == 7) {
+          _key = 3;
+        } else if (_key == 8) {
+          _key = 4;
+        } else if (_key == 9) {
+          continue;
+        } else if (_key == 10) {
+          _key = 6;
+        } else if (_key == 11) {
+          _key = 7;
+        } else if (_key == 12) {
+          _key = 8;
+        } else if (_key == 13) {
+          _key = 11;
+        }
       } else if (direction == 'D') {
-        int[] indices = mapKeyToIndices(_key + 3);
-        _key = keypad[indices[0]][indices[1]];
+        if (_key == 1) {
+          _key = 3;
+        } else if (_key == 2) {
+          _key = 6;
+        } else if (_key == 3) {
+          _key = 7;
+        } else if (_key == 4) {
+          _key = 8;
+        } else if (_key == 5) {
+          continue;
+        } else if (_key == 6) {
+          _key = 10;
+        } else if (_key == 7) {
+          _key = 11;
+        } else if (_key == 8) {
+          _key = 12;
+        } else if (_key == 9) {
+          continue;
+        } else if (_key == 10) {
+          continue;
+        } else if (_key == 11) {
+          _key = 13;
+        } else if (_key == 12) {
+          continue;
+        } else if (_key == 13) {
+          continue;
+        }
       } else if (direction == 'L') {
-        // out of bounds check
-        if (_key % 3 == 1) {
+        if (_key == 1) {
+          continue;
+        } else if (_key == 2) {
+          continue;
+        } else if (_key == 3) {
+          _key = 2;
+        } else if (_key == 4) {
+          _key = 3;
+        } else if (_key == 5) {
+          continue;
+        } else if (_key == 6) {
+          _key = 5;
+        } else if (_key == 7) {
+          _key = 6;
+        } else if (_key == 8) {
+          _key = 7;
+        } else if (_key == 9) {
+          _key = 8;
+        } else if (_key == 10) {
+          continue;
+        } else if (_key == 11) {
+          _key = 10;
+        } else if (_key == 12) {
+          _key = 11;
+        } else if (_key == 13) {
           continue;
         }
-        int[] indices = mapKeyToIndices(_key - 1);
-        _key = keypad[indices[0]][indices[1]];
       } else if (direction == 'R') {
-        // out of bounds check
-        if (_key % 3 == 0) {
+        if (_key == 1) {
+          continue;
+        } else if (_key == 2) {
+          _key = 3;
+        } else if (_key == 3) {
+          _key = 4;
+        } else if (_key == 4) {
+          continue;
+        } else if (_key == 5) {
+          _key = 6;
+        } else if (_key == 6) {
+          _key = 7;
+        } else if (_key == 7) {
+          _key = 8;
+        } else if (_key == 8) {
+          _key = 9;
+        } else if (_key == 9) {
+          continue;
+        } else if (_key == 10) {
+          _key = 11;
+        } else if (_key == 11) {
+          _key = 12;
+        } else if (_key == 12) {
+          continue;
+        } else if (_key == 13) {
           continue;
         }
-        int[] indices = mapKeyToIndices(_key + 1);
-        _key = keypad[indices[0]][indices[1]];
       } else {
         System.out.println(direction);
       }
